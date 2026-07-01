114fc9b922e0f25783c7cf0748fdbc5b2b4c10bc
Mike Orozco <me@mikeorozco.dev>
2026-07-01 10:54:47 -0500

    Standardize app shell width

--- apps/web/app/layouts/wide.vue
+++ apps/web/app/layouts/wide.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="min-h-screen mm-scaled">
-    <AppHeader wide />
-    <main class="mx-auto max-w-[1600px] px-4 py-6 sm:px-6 lg:px-8">
+    <AppHeader />
+    <main class="mm-shell-container px-4 py-6 sm:px-6 lg:px-8">
       <slot />
     </main>
   </div>
